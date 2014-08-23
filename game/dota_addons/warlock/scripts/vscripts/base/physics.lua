188c711ac1c3eb21187e4fbbb1eb62bf848582c8
zymoran <zymoran@gmail.com>
2014-08-24 00:11:47 +0200

    added prototype of wc3-like unit/pillar physics

--- game/dota_addons/warlock/scripts/vscripts/base/physics.lua
+++ game/dota_addons/warlock/scripts/vscripts/base/physics.lua
@@ -336,7 +336,43 @@ function Game:_physResolveCollisions(dt)
 
 		-- perform ellastic if needed
 		if coll_spec.ellastic then
-			self:_physEllasticCollision(coll_spec.cc1, coll_spec.cc2, coll_info)
+			if coll_spec.cc1.channel == 2 or coll_spec.cc2.channel == 2 then
+				self:_physEllasticCollision(coll_spec.cc1, coll_spec.cc2, coll_info)
+			else
+				a1:moveInTime(-Config.GAME_TICK_RATE)
+				a2:moveInTime(-Config.GAME_TICK_RATE) -- revert time to check if player was previously colliding
+				
+				local dif = a2.location - a1.location
+				local dist = dif:Length() -- previous distance
+				local real r2 = coll_spec.cc1.radius + coll_spec.cc2.radius
+				local real s = dist*dist
+				local real r = s - r2*r2
+				
+				a1:moveInTime(Config.GAME_TICK_RATE)
+				a2:moveInTime(Config.GAME_TICK_RATE) -- players can still walk while standing on each other
+				--coll_info.hit_distance = diff:Length()
+			
+				--print(r)
+				if r < 0 then
+					s = math.sqrt(-r)
+					if s < 30 then
+						s = 30
+					end
+					if coll_spec.cc1.channel == coll_spec.cc2.channel then -- warlock-warlock collision
+						a1.location = a1.location - dif/s -- Warlock-Warlock push away
+						a2.location = a2.location + dif/s
+					elseif coll_spec.cc1.channel + coll_spec.cc2.channel == 4 then -- 3+1 = 1+3 = 4 = warlock pillar collision
+						if coll_spec.cc1.channel == 1 then
+							a1.location = a2.location - coll_info.hit_normal*r2	
+						else
+							a2.location = a1.location + coll_info.hit_normal*r2
+						end
+					end
+				else
+					self:_physEllasticCollision(coll_spec.cc1, coll_spec.cc2, coll_info)
+				end
+			end
+			--PrintTable(coll_spec.cc2)			
 		end
 
 		-- revert the time
@@ -344,6 +380,7 @@ function Game:_physResolveCollisions(dt)
 			a1:moveInTime(-tm)
 			a2:moveInTime(-tm)
 		end
+
 
 		if a1.static then
 			a1.velocity = v_zero
