208617258a51e491e9bf744a3072bd9fe46a0af9
Robin Kahlow <xtremegosugaming@googlemail.com>
2015-03-01 14:21:09 +0100

    magnetize self hit after pillar hit, fixed WW cooldown bug, fixed item levels

--- game/dota_addons/warlock/scripts/vscripts/warlock/projectiles/magnetizeprojectile.lua
+++ game/dota_addons/warlock/scripts/vscripts/warlock/projectiles/magnetizeprojectile.lua
@@ -14,29 +14,41 @@ function MagnetizeProjectile:init(def)
 	self.end_sound = def.end_sound
 	self.duration = def.duration
     self.native_mod = def.native_mod
+    self.bounced = false
 
     -- Enable collision with allies and obstacles
     local cc = self.collision_components["projectile"]
 	cc.coll_mat[Player.ALLIANCE_ALLY][CollisionComponent.CHANNEL_PLAYER] = true
+    cc.coll_mat[Player.ALLIANCE_SELF][CollisionComponent.CHANNEL_PLAYER] = true
+    cc.coll_mat[Player.ALLIANCE_ENEMY][CollisionComponent.CHANNEL_OBSTACLE] = true
+    cc.coll_initiative = -1
 end
 
 function MagnetizeProjectile:onCollision(coll_info, cc)
 	local actor = coll_info.actor
 
 	if actor:instanceof(Pawn) then
+        local alliance = actor.owner:getAlliance(self.instigator.owner)
+
+        -- Only self-hit after a bounce
+        if alliance == Player.ALLIANCE_SELF and not self.bounced then
+            return
+        end
+
 		-- Play a hit sound
 		if self.hit_sound then
 			actor.unit:EmitSound(self.hit_sound)
 		end
 
         local time = 0
-        local sign = 0
-        if actor.owner:getAlliance(self.instigator.owner) == Player.ALLIANCE_ALLY then
-            sign = -1
-            time = actor:getBuffDuration(self.duration, self.instigator)
-        else
+        local sign = 0 -- Repel or attract
+
+        if alliance == Player.ALLIANCE_ENEMY then
             sign = -1
             time = actor:getDebuffDuration(self.duration, self.instigator)
+        else
+            sign = 1
+            time = actor:getBuffDuration(self.duration, self.instigator)
         end
 
 		-- Add the grip modifier
@@ -47,8 +59,16 @@ function MagnetizeProjectile:onCollision(coll_info, cc)
 			end_sound = self.end_sound,
             native_mod = self.native_mod,
 		})
-	end
 
-	-- destroy
-	self:setLifetime(0)
+        -- destroy
+	    self:setLifetime(0)
+    elseif(actor:instanceof(Obstacle)) then
+		if self:isMovingTowards(coll_info.hit_normal) then
+			self:reflectVelocity(coll_info.hit_normal)
+            self.bounced = true
+		end
+	else
+        -- destroy
+	    self:setLifetime(0)
+    end
 end
