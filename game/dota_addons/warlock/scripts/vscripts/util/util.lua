8378974fa766b5faa813fc37323484694f866401
ToraxXx <xtremegosugaming@googlemail.com>
2015-11-03 23:08:12 +0100

    added rand_normal to util

--- game/dota_addons/warlock/scripts/vscripts/util/util.lua
+++ game/dota_addons/warlock/scripts/vscripts/util/util.lua
@@ -22,3 +22,29 @@ function copy_extend(t_to_copy, t_extensions)
     extend_table(c, t_extensions)
     return c
 end
+
+-- Normal Distribution
+local generate = false
+local z0 = 0
+local z1 = 0
+
+function rand_normal(mean, stddev)
+    generate = not generate
+
+    if not generate then
+        return z1 * stddev + mean
+    end
+
+    local u1 = 0
+    local u2 = 0
+    repeat
+        u1 = math.random()
+        u2 = math.random()
+    until u1 > 0.0001
+
+    local x = math.sqrt(-2.0 * math.log(u1)) 
+
+    z0 = x * math.cos(2 * math.pi * u2)
+    z1 = x * math.sin(2 * math.pi * u2);
+    return z0 * stddev + mean;
+end
