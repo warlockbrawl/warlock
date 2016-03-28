e44fb6b00673cc92c359e5d1c3477a5235ea9501
Robin Kahlow <robinkahlow@web.de>
2016-03-28 15:30:30 +0200

    web api post instead of get parameters, set web api version to 5

--- game/dota_addons/warlock/scripts/vscripts/base/webapi.lua
+++ game/dota_addons/warlock/scripts/vscripts/base/webapi.lua
@@ -7,12 +7,17 @@ function WebAPI:init(base_url, mod_id, mod_version)
     self.match_token = nil
 end
 
--- Sends a GET request for a path and optionally calls a callback with the received data
+-- Sends a POST request for a path and optionally calls a callback with the received data
 -- Handles the JSON status
-function WebAPI:send(path, callback)
-    local request = CreateHTTPRequest("GET", self.base_url .. path)
+function WebAPI:send(path, params, callback)
+    local request = CreateHTTPRequest("POST", self.base_url .. "/" .. path)
 
-    log("WebAPI sending request, path: " .. path)
+    log("WebAPI sending request, path: " .. path .. " and params:")
+    PrintTable(params)
+
+    for k, v in pairs(params) do
+        request:SetHTTPRequestGetOrPostParameter(tostring(k), tostring(v))
+    end
 
     request:Send(function(result)
         if not result or result.StatusCode ~= 200 then
@@ -36,18 +41,23 @@ function WebAPI:send(path, callback)
     end)
 end
 
-function WebAPI:matchSend(func, path, callback)
+function WebAPI:matchSend(func, params, callback)
     if not self.match_token then
         log("WebAPI tried to call matchSend but no match token was set")
         return
     end
 
-    self:send("/" .. func .. "/" .. self.match_token .. path, callback)
+    params.match_token = self.match_token
+
+    self:send(func, params, callback)
 end
 
 -- Requests a new match id
 function WebAPI:startMatch()
-    self:send("/startmatch/" .. self.mod_id .. "/" .. self.mod_version, function(result)
+    self:send("startmatch", {
+        mod_name = self.mod_id,
+        mod_version = self.mod_version,
+    }, function(result)
         self.match_token = result.data.match_token
         log("WebAPI match successfully created, match token: " .. self.match_token)
     end)
@@ -55,36 +65,54 @@ end
 
 -- Adds players to the current match id
 function WebAPI:addPlayer(steam_id)
-    self:matchSend("addplayer", "/" .. steam_id, function(result)
+    self:matchSend("addplayer", {
+        steam_id = steam_id,
+    }, function(result)
         log("WebAPI successfully added player with steam id " .. steam_id)
     end)
 end
 
 -- Ends the current match
 function WebAPI:finishMatch()
-    self:matchSend("finishmatch", "")
+    self:matchSend("finishmatch", {}, nil)
 end
 
 function WebAPI:setMatchProperty(key, value)
-    self:matchSend("setmatchproperty", "/" .. key .. "/" .. value, function(result)
+    self:matchSend("setmatchproperty", {
+        property_name = key,
+        property_value = value,
+    }, function(result)
         log("WebAPI successfully set match property with key " .. key .. " to " .. value)
     end)
 end
 
 function WebAPI:setMatchPlayerProperty(steam_id, key, value)
-    self:matchSend("setmatchplayerproperty", "/" .. steam_id .. "/" .. key .. "/" .. value, function(result)
+    self:matchSend("setmatchplayerproperty", {
+        steam_id = steam_id,
+        property_name = key,
+        property_value = value,
+    }, function(result)
         log("WebAPI successfully set match player for " .. steam_id .. "'s property with key " .. key .. " to " .. value)
     end)
 end
 
 function WebAPI:setPlayerProperty(steam_id, key, value)
-    self:send("/setplayerproperty/" .. self.mod_id .. "/" .. steam_id .. "/" .. key .. "/" .. value, function(result)
+    self:send("setplayerproperty", {
+        mod_name = self.mod_id,
+        steam_id = steam_id,
+        property_name = key,
+        property_value = value,    
+    }, function(result)
         log("WebAPI successfully set player for " .. steam_id .. "'s property with key " .. key .. " to " .. value)
     end)
 end
 
 function WebAPI:getPlayerProperty(steam_id, key, callback)
-    self:send("/getplayerproperty/" .. self.mod_id .. "/" .. steam_id .. "/" .. key, function(result)
+    self:send("getplayerproperty", {
+        mod_name = self.mod_id,
+        steam_id = steam_id,
+        property_name = key
+    }, function(result)
         local property_value = result.data.property_value
         log("WebAPI successfully got player property with steam id " .. steam_id .. " and key " .. key .. ": " .. property_value)
         callback(steam_id, key, result.data.property_value)
