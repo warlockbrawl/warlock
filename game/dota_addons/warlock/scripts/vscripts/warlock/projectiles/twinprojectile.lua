58863a76f9004e508a1b62d535eaf4b826f679f6
Robin Kahlow <robinkahlow@web.de>
2018-03-04 14:50:09 +0100

    in twin using ellipse(t) instead of its derivative to set velocity to avoid integration errors

--- game/dota_addons/warlock/scripts/vscripts/warlock/projectiles/twinprojectile.lua
+++ game/dota_addons/warlock/scripts/vscripts/warlock/projectiles/twinprojectile.lua
@@ -15,9 +15,10 @@ function TwinProjectile:init(def)
 
 	self.ellipse_sign = def.ellipse_sign
 
-	self.ellipse_angle = math.atan2(delta.y, delta.x)
+	self.ellipse_dir = delta:Normalized()
 
-	self.virtual_velocity = Vector(0, 0, 0)
+	self.virtual_location = self:getEllipseLocation() -- unrotated ellipse location
+	self.prev_unrotated_velocity = Vector(0, 0, 0) -- previously applied unrotated velocity
 
 	self.speed = def.speed
 end
@@ -25,18 +26,19 @@ end
 function TwinProjectile:onCollision(coll_info, cc)
     TwinProjectile.super.onCollision(self, coll_info, cc)
 
+	-- Deal damage
+	coll_info.actor:receiveDamage{
+		source		= self.instigator or self,
+		hit_normal	= coll_info.hit_normal,
+		amount = self.damage or 0,
+	}
+
 	if coll_info.actor:instanceof(Pawn) then
-		-- Deal damage
-		coll_info.actor:receiveDamage{
-			source		= self.instigator or self,
-			hit_normal	= coll_info.hit_normal,
-			amount = self.damage or 0,
-		}
-		
 		-- Spawn an effect
 		local effect = Effect:create(self.hit_effect, { location=coll_info.actor.location })
 		effect:setControlPoint(0, coll_info.actor.location)
 		effect:setControlPoint(1, coll_info.actor.location)
+		effect:setControlPoint(2, coll_info.actor.location)
 
 		-- Play a sound
 		-- TODO: maybe not use the effects locust
@@ -48,30 +50,60 @@ function TwinProjectile:onCollision(coll_info, cc)
 	self:setLifetime(0)
 end
 
-function TwinProjectile:onPreTick(dt)
-	local vx = -self.ellipse_a * math.sin(self.ellipse_t)
-	local vy = self.ellipse_sign * self.ellipse_b * math.cos(self.ellipse_t)
+function TwinProjectile:getEllipseLocation()
+	return Vector(self.ellipse_a * math.cos(self.ellipse_t),
+                  self.ellipse_sign * self.ellipse_b * math.sin(self.ellipse_t),
+                  0)
+end
 
-	local ellipse_dt = dt * self.speed / math.sqrt(vx*vx + vy*vy)
+function TwinProjectile:getEllipseVelocity()
+	return Vector(-self.ellipse_a * math.sin(self.ellipse_t),
+	              self.ellipse_sign * self.ellipse_b * math.cos(self.ellipse_t),
+                  0)
+end
+
+function TwinProjectile:rotateEllipseVector(vec)
+	return Vector(self.ellipse_dir[1] * vec[1] - self.ellipse_dir[2] * vec[2],
+				  self.ellipse_dir[2] * vec[1] + self.ellipse_dir[1] * vec[2],
+				  0)
+end
+
+function TwinProjectile:onPreTick(dt)
+	-- Calculate d Ellipse(t) / dt to determine timestep needed for constant velocity
+	local ellipse_vel = self:getEllipseVelocity()
+
+	local ellipse_dt = dt * self.speed / ellipse_vel:Length()
 	ellipse_dt = max(0.001, ellipse_dt)
 
-	self.ellipse_t = self.ellipse_t + ellipse_dt
+	-- Advance ellipse time by determined timestep
+	self.ellipse_t = min(math.pi, self.ellipse_t + ellipse_dt)
 
-	-- Rotate the ellipse
-	local vx_rot = math.cos(self.ellipse_angle) * vx - math.sin(self.ellipse_angle) * vy
-	local vy_rot = math.sin(self.ellipse_angle) * vx + math.cos(self.ellipse_angle) * vy
+	-- Calculate new location on ellipse at Ellipse(t)
+	local ellipse_loc = self:getEllipseLocation()
 
-	local new_virtual_velocity = Vector(vx_rot, vy_rot, 0) * ellipse_dt / dt
+	-- Calculate velocity required to move from old to new ellipse location
+	-- Potential issue if dt is not fixed every frame
+	local unrotated_velocity = (ellipse_loc - self.virtual_location) / dt
+	self.virtual_location = ellipse_loc
 
-	self.velocity = self.velocity - self.virtual_velocity + new_virtual_velocity
+	-- Rotate the velocity vectors
+	local new_applied_velocity = self:rotateEllipseVector(unrotated_velocity)
+	local old_applied_velocity = self:rotateEllipseVector(self.prev_unrotated_velocity)
 
-	self.virtual_velocity = new_virtual_velocity
+	self.prev_unrotated_velocity = unrotated_velocity
 
-	if self.ellipse_t >= 1.07 * math.pi then
+	-- Subtract old applied velocity and add the new one
+	self.velocity = self.velocity - old_applied_velocity + new_applied_velocity
+
+	-- Destroy if end of ellipse was reached
+	if self.ellipse_t >= math.pi then
 		self:setLifetime(0)
 	end
 end
 
-function TwinProjectile:negateAngle()
-	self.ellipse_angle = -self.ellipse_angle
+function TwinProjectile:reflectVelocity(normal)
+	TwinProjectile.super.reflectVelocity(self, normal)
+
+	-- Reflect the ellipse direction too
+	self.ellipse_dir = self.ellipse_dir - 2 * normal:Dot(self.ellipse_dir) * normal
 end
